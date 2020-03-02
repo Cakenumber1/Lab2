62a2f2a3669fbec5b32feaf9d5c21456329b344e
Misha <cakenumber1@mail.ru>
2020-03-03 02:03:17 +0300

    1

--- Func.cs
+++ Func.cs
@@ -43,5 +43,17 @@ namespace Calc
         /// </summary>
         /// <value>Gets 1 double, returns 1 double.</value>
         public double Fac(double a) { int ans=1; for (int i = 1; i <= a; i++) { ans *= i; } return ans; }
+        /// <summary>
+        /// Counts sin of an angle.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public double Sin(double a) { return Math.Sin(a); }
+        /// <summary>
+        /// Counts cos of an angle.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public double Cos(double a) { return Math.Cos(a); }
     }
 }
